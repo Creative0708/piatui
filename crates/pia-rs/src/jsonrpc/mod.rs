03a974c9ed9058ae3410e4e8c4c2d8a443053d13
Colin Cai <therealcreative0708@gmail.com>
2024-07-14 23:42:01 -0400

    async wip

--- crates/pia-rs/src/jsonrpc/mod.rs
+++ crates/pia-rs/src/jsonrpc/mod.rs
@@ -1,4 +1,10 @@
-use std::io::{self, Read};
+//! # Implementation of Private Internet Access's IPC Layer
+//!
+//! Private Internet Access uses JSON-RPC through a custom IPC layer. This layer allows for resynchronization in case a message is incompletely transmitted.
+//!
+//! This module is a rough translation of its C++ code to Rust. Go check out the [explanation](https://github.com/pia-foss/desktop/blob/522751571ea7f6b1a9e3dd5cc4c70fc2fd136221/common/src/ipc.cpp#L33) in the PIA repo for more details.
+
+use async_std::io::{self};
 
 cfg_if::cfg_if! {
     if #[cfg(unix)] {
@@ -9,32 +15,37 @@ cfg_if::cfg_if! {
     }
 }
 
-pub trait DaemonJSONRPCConnectionTrait: io::Read + io::Write {
-    fn new() -> io::Result<Self>
-    where
-        Self: Sized;
+pub trait DaemonJSONRPCConnectionTrait: io::Read + io::Write + Clone {
+    async fn new() -> io::Result<Self>;
 }
 
 const _: () = {
     const fn sanity_check<T: DaemonJSONRPCConnectionTrait>() {}
-    sanity_check::<PlatformDaemonConnection>()
+    sanity_check::<PlatformDaemonConnection>();
 };
 
-pub struct DaemonJSONRPCConnection {
-    inner: PlatformDaemonConnection,
+// Local socket magic number. always sent at the start of each frame
+const PIA_LOCAL_SOCKET_MAGIC: [u8; 4] = 0xFFACCE56u32.to_be_bytes();
+
+pub async fn connect() -> io::Result<(DaemonJSONRPCReciever, DaemonJSONRPCSender)> {
+    let inner:  = PlatformDaemonConnection::new().await?;
+    Ok(Self {
+        reader: DaemonJSONRPCReciever::new(inner.clone()),
+        writer: DaemonJSONRPCSender::new(inner),
+    })
 }
 
-impl DaemonJSONRPCConnection {
-    pub fn new() -> io::Result<Self> {
-        let mut inner = PlatformDaemonConnection::new()?;
-
-        let mut buf = [0; 1024];
-        let mut size = inner.read(&mut buf)?;
-
-        println!("{:?}", &buf[0..size]);
-
-        todo!();
-
-        // Ok(Self { inner })
+pub struct DaemonJSONRPCReciever(PlatformDaemonConnection);
+impl DaemonJSONRPCReciever {
+    fn new(reader: PlatformDaemonConnectionReader) -> Self {
+        Self(reader)
     }
+    fn  
+}
+pub struct DaemonJSONRPCSender(PlatformDaemonConnection);
+impl DaemonJSONRPCSender {
+    fn new(writer: PlatformDaemonConnectionWriter) -> Self {
+        Self(writer)
+    }
+    fn  
 }
--- crates/pia-rs/src/jsonrpc/unix.rs
+++ crates/pia-rs/src/jsonrpc/unix.rs
@@ -1,45 +1,10 @@
-use std::{
-    io::{self, BufReader, BufWriter},
-    os::unix::net::UnixStream,
-};
+use async_std::{io, os::unix::net::UnixStream};
 
-pub struct UnixSocketDaemonConnection(BufReader<UnixStream>, BufWriter<UnixStream>);
-
-impl io::Read for UnixSocketDaemonConnection {
-    fn read(&mut self, buf: &mut [u8]) -> io::Result<usize> {
-        self.0.read(buf)
-    }
-    fn read_exact(&mut self, buf: &mut [u8]) -> io::Result<()> {
-        self.0.read_exact(buf)
-    }
-    fn read_to_end(&mut self, buf: &mut Vec<u8>) -> io::Result<usize> {
-        self.0.read_to_end(buf)
-    }
-    fn read_to_string(&mut self, buf: &mut String) -> io::Result<usize> {
-        self.0.read_to_string(buf)
-    }
-    fn read_vectored(&mut self, bufs: &mut [io::IoSliceMut<'_>]) -> io::Result<usize> {
-        self.0.read_vectored(bufs)
-    }
-}
-impl io::Write for UnixSocketDaemonConnection {
-    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
-        self.1.write(buf)
-    }
-    fn write_vectored(&mut self, bufs: &[io::IoSlice<'_>]) -> io::Result<usize> {
-        self.1.write_vectored(bufs)
-    }
-    fn flush(&mut self) -> io::Result<()> {
-        self.1.flush()
-    }
-}
+// too lazy for newtype
+pub type UnixSocketDaemonConnection = UnixStream;
 
 impl super::DaemonJSONRPCConnectionTrait for UnixSocketDaemonConnection {
-    fn new() -> io::Result<Self> {
-        let stream = UnixStream::connect("/opt/piavpn/var/daemon.sock")?;
-        Ok(Self(
-            BufReader::new(stream.try_clone()?),
-            BufWriter::new(stream),
-        ))
+    async fn new() -> io::Result<Self> {
+        Self::connect("/opt/piavpn/var/daemon.sock").await
     }
 }
