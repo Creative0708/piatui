7785560770671063a069b53d4b0d75dcfb4b0d42
Colin Cai <therealcreative0708@gmail.com>
2024-07-18 17:27:46 -0400

    switch _back_ to sync and make wip daemon connection

--- crates/pia-rs/src/jsonrpc/mod.rs
+++ crates/pia-rs/src/jsonrpc/mod.rs
@@ -4,23 +4,15 @@
 //!
 //! This module is a rough translation of its C++ code to Rust. Go check out the [explanation](https://github.com/pia-foss/desktop/blob/522751571ea7f6b1a9e3dd5cc4c70fc2fd136221/common/src/ipc.cpp#L33) in the PIA repo for more details.
 
-use std::mem::MaybeUninit;
-
-use async_std::io::{self, ReadExt};
+use std::io::{self, Read};
 
 cfg_if::cfg_if! {
     if #[cfg(unix)] {
         pub mod unix;
-        pub type PlatformDaemonConnection = unix::UnixSocketDaemonConnection;
+        pub type PlatformDaemonConnectionReader = unix::UnixSocketDaemonConnection;
     } else {
         compile_error!("platform not implemented D:");
     }
-}
-
-pub trait DaemonJSONRPCConnectionTrait: io::Read + io::Write {
-    async fn new() -> io::Result<Self>
-    where
-        Self: Sized;
 }
 
 const _: () = {
@@ -35,44 +27,61 @@ const PIA_LOCAL_SOCKET_MAGIC: [u8; 4] = 0xFFACCE56u32.to_be_bytes();
 const VALID_MESSAGE_SIZES: std::ops::RangeInclusive<u32> = 2..=1024 * 1024;
 
 pub struct DaemonJSONRPCConnection {
-    inner: PlatformDaemonConnection,
-    last_server_seq_num: u16,
+    pub receiver: DaemonJSONRPCReceiver,
+    pub sender: DaemonJSONRPCSender,
 }
 
-impl DaemonJSONRPCConnection {
-    pub async fn new() -> io::Result<Self> {
+pub struct DaemonJSONRPCReceiver {
+    inner: PlatformDaemonConnection,
+}
+
+impl DaemonJSONRPCReceiver {
+    pub fn new() -> io::Result<Self> {
         Ok(Self {
-            inner: PlatformDaemonConnection::new().await?,
+            inner: PlatformDaemonConnection::new()?,
+            last_server_ack: None,
+            last_sent_seq: None,
         })
     }
 
-    pub async fn poll(&mut self) -> io::Result<Vec<u8>> {
-        loop {}
+    pub fn poll(&mut self) -> io::Result<Vec<u8>> {
+        loop {
+            let (seq_num, msg) = self.poll_raw()?;
+            if msg.is_empty() {
+                // message is an acknowledgement message; ignore
+                self.last_server_ack = Some(seq_num);
+            } else {
+                break Ok(msg);
+            }
+        }
     }
 
-    async fn poll_raw(&mut self) -> io::Result<Vec<u8>> {
+    /// Polls a message from the connection and returns the sequence number and its contents.
+    fn poll_raw(&mut self) -> io::Result<(u16, Vec<u8>)> {
         // message header
-        let mut header_buf = [0; 12];
-        self.inner.read_exact(&mut header_buf);
-        if header_buf[0..4] != PIA_LOCAL_SOCKET_MAGIC {
+        let mut header_buf = [0u32; 3];
+        self.inner
+            .read_exact(bytemuck::cast_slice_mut(&mut header_buf))?;
+        if header_buf[0].to_ne_bytes() != PIA_LOCAL_SOCKET_MAGIC {
             return Result::Err(io::Error::new(
                 io::ErrorKind::InvalidData,
                 format!(
                     "expected {PIA_LOCAL_SOCKET_MAGIC:?} as header magic number, got {:?}",
-                    &header_buf[0..4]
+                    header_buf[0].to_ne_bytes()
                 ),
             ));
         }
-        let seq_low =
-            u8::from_le_bytes((&header_buf[4..6]).try_into().expect("unreachable")) >> 4 as u8;
-        let seq_high =
-            u16::from_le_bytes((&header_buf[6..8]).try_into().expect("unreachable")) >> 4 as u8;
+        let seq_shorts: [u16; 2] = bytemuck::cast(header_buf[1]);
+        let seq_low = (u16::from_le_bytes(seq_shorts[0].to_ne_bytes()) >> 4) as u8;
+        let seq_high = (u16::from_le_bytes(seq_shorts[1].to_ne_bytes()) >> 4) as u8;
         let seq_num = seq_low as u16 | (seq_high as u16) << 8;
 
-        if self.last_server_seq_num.wrapping_add(1) != seq_num {}
-        self.last_server_seq_num = seq_num;
+        let length = u32::from_le_bytes(header_buf[2].to_ne_bytes());
 
-        let length = u32::from_le_bytes((&header_buf[8..12]).try_into().expect("unreachable"));
+        if length == 0 {
+            return Ok((seq_num, vec![]));
+        }
+
         if !VALID_MESSAGE_SIZES.contains(&length) {
             return Result::Err(io::Error::new(
                 io::ErrorKind::InvalidData,
@@ -80,9 +89,8 @@ impl DaemonJSONRPCConnection {
             ));
         }
 
-        let mut buf = Vec::with_capacity(length as usize);
-        buf.resize(length as usize, 0);
-        self.inner.read_exact(&mut buf).await?;
-        Ok(buf)
+        let mut buf = vec![0; length as usize];
+        self.inner.read_exact(&mut buf)?;
+        Ok((seq_num, buf))
     }
 }
--- crates/pia-rs/src/jsonrpc/unix.rs
+++ crates/pia-rs/src/jsonrpc/unix.rs
@@ -1,10 +1,14 @@
-use async_std::{io, os::unix::net::UnixStream};
+use std::{io, os::unix::net::UnixStream};
+
+pub fn create() -> UnixSocketDaemonConnection {
+    
+}
 
 // too lazy for newtype
 pub type UnixSocketDaemonConnection = UnixStream;
 
 impl super::DaemonJSONRPCConnectionTrait for UnixSocketDaemonConnection {
-    async fn new() -> io::Result<Self> {
-        Self::connect("/opt/piavpn/var/daemon.sock").await
+    fn new() -> io::Result<Self> {
+        Self::connect("/opt/piavpn/var/daemon.sock")
     }
 }
